1c764ce3103454679b690861da9ed461df906eac
Dan Rosén <danr42@gmail.com>
2021-05-04 22:14:27 +0200

    Port commas to python

--- commas.kak
+++ commas.kak
@@ -1,129 +1,19 @@
 # insert mode with commas for symbols
 
-def -hidden with-map -params 1..3 %{
-    # X-macro style https://quuxplusone.github.io/blog/2021/02/01/x-macros/
-    %arg{1} "'" '!'
-    %arg{1}   , ','
-    %arg{1}   . '+'
-    %arg{1}   p '|'
-    %arg{1}   y '%'
+python /home/dan/code/kakconf/commas.py setup
 
-    eval %arg{2}
+map global normal r ': comma-replace<ret>'
 
-    %arg{1}   f '>'
-    %arg{1}   g '{'
-    %arg{1}   c '('
-    %arg{1}   r 'C'
-    %arg{1}   l '}'
-    %arg{1}   / '?'
-    eval %arg{3}
-
-
-    %arg{1}   a '&'
-    %arg{1}   o '@'
-    %arg{1}   e '='
-    %arg{1}   u '_'
-    %arg{1}   i '~'
-    eval %arg{2}
-
-    %arg{1}   d '$'
-    %arg{1}   h '['
-    %arg{1}   t "'"
-    %arg{1}   n '"'
-    %arg{1}   s ']'
-    eval %arg{3}
-
-    %arg{1}   : 'å'
-    %arg{1}   q 'ä'
-    %arg{1}   j 'ö'
-    %arg{1}   k '\'
-    %arg{1}   x '^'
-    eval %arg{2}
-
-    %arg{1}   b '<'
-    %arg{1}   m '*'
-    %arg{1}   w ' '
-    %arg{1}   v '`'
-    %arg{1}   z ';'
-}
-
-try %{ decl -hidden str comma_info '' }
-
-set global comma_info ''
-def -hidden k -params 1..2 %{
-    set -add global comma_info "%arg{1}%arg{2} "
-}
-with-map k %{
-    set -add global comma_info '  '
-} %{
-    set -add global comma_info '
-' }
-
-def save-map %{
-    nop %sh{
-        echo 'm={}' > ~/code/keyboard-mapping/kakmap.py
-    }
-    def -hidden -override k -params 1..2 %{
-        nop %sh{
-            python -c 'if 1:
-                import sys
-                _, a, b = sys.argv
-                if b.strip():
-                    print(f"""m[{a!r}] = {b.replace("C", ")")!r}""")
-            ' "$1" "$2" >> ~/code/keyboard-mapping/kakmap.py
-        }
-    }
-    with-map k
-}
-
-def modal-clear %{
-    info -style modal
-}
-
-def -hidden comma-info %{
-    info -style below -anchor "%val{cursor_line}.%val{cursor_column}" %opt{comma_info}
-}
-
-rmhooks global commas
-hook -group commas global InsertChar , %{
-    hook -group comma-once -once window InsertIdle .* %{
-        comma-info
-    }
-
-    def -hidden -override comma-cleanup %{
-        rmhooks window comma-once
-        modal-clear
-    }
-
-    def -hidden -override k -params 1..2 %{
-        # did we just press $1?
-        hook -group comma-once -once window InsertChar "\Q%arg{1}" "comma-replace-with %arg{2}"
-    }
-
-    def -hidden -override comma-replace-with -params .. %{
-        # replace it with $2 then
-        exec <backspace><backspace> %arg{1}
-        try %{
-            # C calls close instead
-            exec -draft <esc> h <a-k> C <ret> d
-            close
-        }
-        comma-cleanup
-    }
-
-    with-map k
-    hook -group comma-once -once window ModeChange .* comma-cleanup
-    hook -group comma-once -once window InsertChar .* comma-cleanup
-    hook -group comma-once -once window RawKey [^,]+ comma-cleanup
-}
-
-def comma-replace %{
+def -override comma-replace %{
     info -title 'replace with char' 'enter char to replace with'
     on-key %{
         info
         eval %sh{
             if test "$kak_key" = ","; then
-                echo comma-replace-comma
+                printf %s "
+                    info -title 'replace with comma char' 'enter char to replace with'
+                    on-key 'python /home/dan/code/kakconf/commas.py replace'
+                "
             else
                 echo exec r "$kak_key"
             fi
@@ -131,24 +21,3 @@ def comma-replace %{
     }
 }
 
-def comma-replace-comma %{
-    info -title 'replace with char' %opt{comma_info}
-    on-key %{
-        def -hidden -override k -params 1..2 %{
-            eval %sh{
-                if test "$kak_key" = "$1"; then
-                    if test "$2" = "C"; then
-                        echo exec 's.<ret>d'
-                        echo close
-                    else
-                        echo exec r "$2"
-                    fi
-                fi
-            }
-        }
-        with-map k
-        info
-    }
-}
-
-map global normal r ': comma-replace<ret>'
--- commas.py
+++ commas.py
@@ -1,109 +1,17 @@
 
-from libkak import expose_def, define, q
+from textwrap import indent
+import sys
+import re
+import os
 
-expose_def(
-    lambda: print('info ' + os.environ.get('kak_buffile', '?')),
-    'test',
-    ['kak_buffile'],
-    '-override'
-)
-
-expose_def(
-    lambda *args: print(f'''info "{' '.join(args)}"'''),
-    'test2',
-    [],
-    '-params .. -override'
-)
-
-@define
-def test3(*, buffile, reg_dquote):
-    """
-    Do amazing things!
-    """
-    return 'info ' + reg_dquote
-
-@define
-def test4(arg1, *, buffile, reg_dquote):
-    """
-    Do amazing things!
-    """
-    return [
-        q.info('--', reg_dquote),
-        q.echo(buffile, arg1, debug=True),
-    ]
-
-@define
-def test5(arg1, arg2, *args, buffile):
-    return ''
-
-@define
-def test6(arg1, arg2=2, *args, buffile):
-    return ''
-
-@define
-def test7(arg1=1, arg2=2, *args, buffile):
-    return ''
-
-@define
-def test8(arg1, arg2, *, buffile):
-    return ''
-
-@define
-def test9(arg1, arg2=2, *, buffile):
-    return ''
-
-@define
-def test0(arg1=1, arg2=2, *, buffile):
-    return ''
-
-@define
-def snake_case(arg):
-    return q.echo(arg.replace('-','_'))
-
-@define
-def yields():
-    yield q.echo('0-first', debug=True)
-    yield q.echo('1-second', debug=True)
-    yield q.echo('2-third', debug=True)
-
-@define
-def py_impl(f, *, selection):
-    return [
-        q.reg('r', eval(f, globals(), dict(s=selection))),
-        q.exec('"rR'),
-    ]
-
-@define
-def py(f):
-    return q.eval(q.py_impl(f), itersel=True, save_regs='r')
-
-@define
-def py_all(f, *, quoted_selections):
-    import sys
-    import shlex
-    selections = shlex.split(quoted_selections)
-    return q.eval(
-        ';\n'.join(
-            ';\n'.join([
-                q.reg('r', eval(f, globals(), dict(s=s, i=i))),
-                q.exec('<space>"rR', draft=True),
-                q.exec(')'),
-            ])
-            for i, s in enumerate(selections)
-        ),
-        draft=True,
-        save_regs='r',
-    )
-
+from filer import q
 
 maps = {}
 maps[10] = r'''  1 2 3 4 5  6 7 8 9 0    --  ! @ # $ %  ^ & * ( )    --  ! , . $ %  ^ & * ( )    '''
-maps[24] = r'''  ' , . p y  f g c r l /  --  " < > P Y  F G C R L ?  --  ! , + | %  > { ( ) } ?  '''
+maps[24] = r'''  ' , . p y  f g c r l /  --  " < > P Y  F G C R L ?  --  ! , + | %  < { ( ) } ?  '''
 maps[38] = r'''  a o e u i  d h t n s    --  A O E U I  D H T N S    --  & @ = _ ~  $ [ ' " ]    '''
-maps[52] = r'''  : q j k x  b m w v z    --  ; Q J K X  B M W V Z    --  å ä ö \ ^  < * w ` ;    '''
+maps[52] = r'''  : q j k x  b m w v z    --  ; Q J K X  B M W V Z    --  å ä ö \ ^  > * w ` ;    '''
 maps[9]  = r'''  Escape -- Escape -- space  '''
-
-import re
 
 ckm = {}
 for start, row in maps.items():
@@ -116,24 +24,11 @@ for start, row in maps.items():
         if re.match("[a-z:',.]$", a):
             ckm[a] = c
 
-from textwrap import indent
+def setup():
+    yield 'rmhooks global commas'
+    yield 'hook -group commas global InsertChar , ' + q([*hook_on_insert()])
 
-def seq(*cmds):
-    body = '\n' + '\n'.join(cmds)
-    body = indent(body, prefix='  ')
-    return q.eval(body)
-    # if not re.search('[()]', body):
-    #     return 'eval %(' + body + ')'
-    # elif not re.search('[{}]', body):
-    #     return 'eval %{' + body + '}'
-    # elif not re.search(r'[\[\]]', body):
-    #     return 'eval %[' + body + ']'
-    # elif not re.search('[<>]', body):
-    #     return 'eval %<' + body + '>'
-    # else:
-    #     return q.eval(body)
-
-def start_comma():
+def hook_on_insert():
     cleanup = 'rmhooks window comma-once'
     yield cleanup
     for trigger, output in ckm.items():
@@ -141,22 +36,30 @@ def start_comma():
             cmd = 'eval %{exec <backspace><backspace>; close}'
         else:
             cmd = 'exec <backspace><backspace> ' + q(output)
-        cmd = seq(
+        cmd = q('eval', [
             # 'set window debug commands',
             cmd,
             cleanup,
             # 'set window debug ""',
-        )
+        ])
         yield 'hook -group comma-once -once window InsertChar ' + q(r'\Q' + trigger) + ' ' + q(cmd)
     yield 'hook -group comma-once -once window ModeChange .*    ' + q(cleanup)
     yield 'hook -group comma-once -once window InsertChar .*    ' + q(cleanup)
     yield 'hook -group comma-once -once window RawKey     [^,]+ ' + q(cleanup)
 
-@define(style='raw')
-def setup_comma():
-    yield 'rmhooks global commas'
-    yield 'hook -group commas global InsertChar , ' + q(seq(*start_comma()))
+def main(cmd):
+    if cmd == 'setup':
+        print('\n'.join(setup()))
+    elif cmd == 'replace':
+        key = os.environ["kak_key"]
+        repl = ckm.get(key)
+        if repl == 'C':
+            print('exec', 's.<ret>d')
+            print('close')
+        elif repl:
+            print('exec', 'r', q(repl))
+        else:
+            print('exec', 'r', q(key))
 
-@define(style='on-key')
-def hmm(*, key):
-    yield q.echo(key)
+if __name__ == '__main__':
+    main(*sys.argv[1:])
