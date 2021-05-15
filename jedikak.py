58ae861a38e8ae3f61c9f991071eb04545ab5faf
Dan Rosén <danr42@gmail.com>
2021-05-15 10:52:03 +0200

    Simplify jedi

--- jedikak.py
+++ jedikak.py
@@ -7,28 +7,23 @@ pipe_escape = lambda s: s.replace("|", "\\|")
 def jedi_impl(buffile, client, timestamp, line, column, code, info_str, last_name, *args):
     line   = int(line)
     column = int(column)
+    cmds = []
     if args[0] == "complete":
         script = jedi.Script(code=code, path=buffile)
         completions = (
-            pipe_escape(c.name) + "|" +
-            pipe_escape("evaluate-commands " + quote(
-                "set-option buffer jedi_last_name " + quote(c.name) +
-                ";" +
-                "jedi completion-info-request " + quote(c.name)
-            )) + "|" +
-            pipe_escape(c.name)
+            pipe_escape(c.name) +
+            "|" + pipe_escape(quote("evaluate-commands", "jedi completion-info-request")) +
+            "|" + pipe_escape(c.name)
             for c in script.complete(line=line, column=column-1)
         )
         header = str(line) + "." + str(column) + "@" + timestamp
-        cmds = [
+        cmds += [
             quote("echo", "completed"),
             quote("set-option", "buffer=" + buffile, "jedi_info", "{}"),
             quote("set-option", "buffer=" + buffile, "jedi_last_name", ""),
             quote("set-option", "buffer=" + buffile, "jedi_completions", header, *completions),
         ]
     elif args[0] == "completion-info-request":
-        name = args[1]
-        cmds = []
         script = jedi.Script(code=code, path=buffile)
         names = script.goto(line=line, column=column-1)
         if names:
@@ -41,17 +36,17 @@ def jedi_impl(buffile, client, timestamp, line, column, code, info_str, last_nam
         script = jedi.Script(code=code, path=buffile)
         names = script.goto(line=line, column=column-1)
         if names:
-            cmds = ["info -- " + quote(names[0].docstring())]
+            cmds += ["info -- " + quote(names[0].docstring())]
         else:
-            cmds = []
+            cmds += []
     elif args[0] == "goto":
         script = jedi.Script(code=code, path=buffile)
         names = script.infer(line=line, column=column-1)
         if names:
             name = names[0]
-            cmds = [quote("edit", str(name.module_path), str(name.line), str(name.column + 1))]
+            cmds += [quote("edit", str(name.module_path), str(name.line), str(name.column + 1))]
         else:
-            cmds = ["info nope"]
+            cmds += ["info nope"]
     else:
-        cmds = [quote("info", "--", "No command in:" + repr(args))]
+        cmds += [quote("info", "--", "No command in:" + repr(args))]
     return quote("evaluate-commands", "-client", client, "\n".join(cmds))
